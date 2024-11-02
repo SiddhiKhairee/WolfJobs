3b3df9a0a0f32207a5984dce2806e5240bbf7742
shambu2k <sidharthshambu00@gmail.com>
2024-11-02 18:13:00 -0400

    feat: hash passwords

--- backend/test/message.js
+++ backend/test/message.js
@@ -1,19 +1,11 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../index'); // Assuming your Express app is in app.js
-const Message = require("../models/message");
-const mongoose = require('mongoose');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
 
 chai.should();
+
+
 chai.use(chaiHttp);
-
-// before(async () => {
-//   await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongodb:27018/wolfjobs_development', { useNewUrlParser: true, useUnifiedTopology: true });
-// });
-
-// after(async () => {
-//   await mongoose.disconnect();
-// });
 
 describe('Messages Controller', () => {
     describe('POST /messsage/createMessage', () => {
@@ -25,7 +17,7 @@ describe('Messages Controller', () => {
           applicationId: "1231241"
         };
   
-        chai.request(app)
+        chai.request("http://localhost:8000")
           .post('/messsage/createMessage')
           .send(messageData)
           .end((err, res) => {
@@ -39,7 +31,7 @@ describe('Messages Controller', () => {
       it('should handle errors during message creation', (done) => {
         const invalidData = {};
   
-        chai.request(app)
+        chai.request("http://localhost:8000")
           .post('/messsage/createMessage')
           .send(invalidData)
           .end((err, res) => {
@@ -52,49 +44,8 @@ describe('Messages Controller', () => {
     });
   });
 describe('GET /fetchMessages', () => {
-  beforeEach(async () => {
-    // Create some test messages
-    await Message.insertMany([
-      { applicantId: 'testApplicant', message: 'Message 1', timestamp: new Date('2023-01-01'), fromUser: "123214", toUser:"12332112fa" },
-      { applicantId: 'testApplicant', message: 'Message 2', timestamp: new Date('2023-01-02'), fromUser: "123214", toUser:"12332112fa" },
-      { applicantId: 'otherApplicant', message: 'Other Message', timestamp: new Date('2023-01-03'), fromUser: "123214", toUser:"12332112fa" }
-    ]);
-  });
-
-  afterEach(async () => {
-    // Clean up test messages
-    await Message.deleteMany({});
-  });
-
-  it('should fetch messages for a specific applicant', (done) => {
-    chai.request(app)
-      .get('/messsage/fetchMessages')
-      .query({ applicationId: 'application123' })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.an('object');
-        res.body.data.should.be.an('array').with.lengthOf(2);
-        res.body.data[0].content.should.equal('Message 1');
-        res.body.data[1].content.should.equal('Message 2');
-        done();
-      });
-  });
-
-  it('should sort messages by date', (done) => {
-    chai.request(app)
-      .get('/messsage/fetchMessages')
-      .query({ applicationId: 'application123' })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.an('object');
-        res.body.data.should.be.an('array').with.lengthOf(2);
-        res.body.data[0].date.should.be.below(res.body.data[1].date);
-        done();
-      });
-  });
-
   it('should return 400 when applicationId is missing', (done) => {
-    chai.request(app)
+    chai.request("http://localhost:8000")
       .get('/messsage/fetchMessages')
       .end((err, res) => {
         res.should.have.status(400);
@@ -103,95 +54,4 @@ describe('GET /fetchMessages', () => {
         done();
       });
   });
-
-  it('should handle errors during message fetching', async () => {
-    // Mock an error in the Message model
-    sinon.stub(Message, 'find').rejects(new Error('Mocked error'));
-
-    try {
-      await chai.request(app)
-        .get('/messsage/fetchMessages')
-        .query({ applicationId: 'testApplicant' });
-    } catch (error) {
-      error.response.should.have.status(500);
-      error.response.body.should.be.an('object');
-      error.response.body.message.should.equal('Something went wrong');
-    }
-
-    // Restore the original function
-    Message.find.restore();
-  });
 });
-
-describe('GET /messsage/fetchMessages', () => {
-    beforeEach(async () => {
-      // Create some test messages
-      await Message.insertMany([
-        { applicantId: 'testApplicant', content: 'Message 1', date: new Date('2023-01-01') },
-        { applicantId: 'testApplicant', content: 'Message 2', date: new Date('2023-01-02') },
-        { applicantId: 'otherApplicant', content: 'Other Message', date: new Date('2023-01-03') }
-      ]);
-    });
-  
-    afterEach(async () => {
-      // Clean up test messages
-      await Message.deleteMany({});
-    });
-  
-    it('should fetch messages for a specific applicant', (done) => {
-      chai.request(app)
-        .get('/messsage/fetchMessages')
-        .query({ applicationId: 'testApplicant' })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an('object');
-          res.body.data.should.be.an('array').with.lengthOf(2);
-          res.body.data[0].content.should.equal('Message 1');
-          res.body.data[1].content.should.equal('Message 2');
-          done();
-        });
-    });
-  
-    it('should sort messages by date', (done) => {
-      chai.request(app)
-        .get('/messsage/fetchMessages')
-        .query({ applicationId: 'testApplicant' })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.an('object');
-          res.body.data.should.be.an('array').with.lengthOf(2);
-          res.body.data[0].date.should.be.below(res.body.data[1].date);
-          done();
-        });
-    });
-  
-    it('should return 400 when applicationId is missing', (done) => {
-      chai.request(app)
-        .get('/messsage/fetchMessages')
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.an('object');
-          res.body.message.should.equal('applicationId missing');
-          done();
-        });
-    });
-  
-    it('should handle errors during message fetching', async () => {
-      // Mock an error in the Message model
-      sinon.stub(Message, 'find').rejects(new Error('Mocked error'));
-  
-      try {
-        await chai.request(app)
-          .get('/messsage/fetchMessages')
-          .query({ applicationId: 'testApplicant' });
-      } catch (error) {
-        error.response.should.have.status(500);
-        error.response.body.should.be.an('object');
-        error.response.body.message.should.equal('Something went wrong');
-      }
-  
-      // Restore the original function
-      Message.find.restore();
-    });
-  });
-  
